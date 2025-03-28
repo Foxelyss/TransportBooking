fd5215c4a3e007b9b92f262f2a25c24e38e3329e
Степан Смирнов <foxelyss@gmail.com>
2025-03-28 10:04:49 +0700

    Первые Unit тесты для бронирования

---
+++ src/main/resources/data-booking-test.sql
@@ -0,0 +1,13 @@
+insert into point values (null,'г. Томск','Томская область','Томск');
+insert into point values (null,'г. Новосибирск','Новосибирская область','Новосибирск') ;
+insert into point values (null,'г. Санкт-Петербург','Санкт-Петербург','Санкт-Петербург') ;
+insert into point values (null,'г. Севастополь', 'Севастополь', 'Севастополь');
+
+insert into Company values (null,'ООО Лана','123123','г. Томск', 'Домашний номер: 12-34-56 Обычный: 8 963 312 75 66');
+insert into Company values (null,'ООО РЖД', '12312389', 'г. Москва', 'Домашний номер: 12-31-56 Обычный: 8 123 312 75 66');
+
+insert into TransportingMeans values(null,'Ж/Д');
+insert into TransportingMeans values(null,'Автобусом');
+
+insert into transportation values (null, 'Будущее', unixepoch()+86400, unixepoch()+86400+86400, 1, 2, 1, 1, 26354, 50, 50);
+insert into transportation values (null, 'Прошлое', unixepoch()-86400-86400, unixepoch()-86400, 1, 2, 1, 1, 26354, 50, 50);
--- src/test/java/com/foxelyss/transportbooking/service/BookServiceTest.java
+++ src/test/java/com/foxelyss/transportbooking/service/BookServiceTest.java
@@ -1,26 +1,71 @@
 package com.foxelyss.transportbooking.service;
 
-import com.foxelyss.transportbooking.model.Book;
 import com.foxelyss.transportbooking.model.Passenger;
-import com.foxelyss.transportbooking.repos.PassengerRepo;
+import com.foxelyss.transportbooking.repos.BookRepo;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ComponentScan({"com.foxelyss.transportbooking.model"})
+@ActiveProfiles("test")
+@Sql("/data-booking-test.sql")
 class BookServiceTest {
     @Autowired
-    BookService passengerRepo;
+    BookService bookService;
+
+    @Autowired
+    TransportService transportService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    final Passenger testificate = new Passenger(8_900_920_07_23L, "ivanovich@sibmail.ru", "Иванов", "Иван", "Иванович", 123213_1231);
 
     @Test
-    void saveAndRequest() {
-//        passengerRepo.save(new Passenger(8_900_920_07_23L, "ivanovich@sibmail.ru", "Иванов", "Иван", "Иванович", 123213_1231));
-        Passenger testificate = new Passenger(8_900_920_07_23L, "ivanovich@sibmail.ru", "Иванов", "Иван", "Иванович", 123213_1231);
-        passengerRepo.createItem(testificate, 1);
+    void shouldBook() {
+        bookService.createItem(testificate, 1);
 
-        passengerRepo.findAllByDetails("ivanovich@sibmail.ru", 123213_1231);
+        List<BookRepo.Ticket> tickets = bookService.findAllByDetails(testificate.Email(), testificate.passport());
+
+        assertEquals(1, tickets.size());
+        assertEquals(1, tickets.getFirst().transporting());
     }
 
+    @Test
+    void shouldReturnBook() {
+        bookService.createItem(testificate, 1);
+        List<BookRepo.Ticket> tickets = bookService.findAllByDetails(testificate.Email(), testificate.passport());
+
+        bookService.deleteItem(testificate.Email(), testificate.passport(), tickets.getFirst().id());
+        tickets = bookService.findAllByDetails(testificate.Email(), testificate.passport());
+
+        assertEquals(0, tickets.size());
+    }
+
+    @Test
+    void shouldNotReturnBook() {
+        bookService.createItem(testificate, 2);
+        List<BookRepo.Ticket> tickets = bookService.findAllByDetails(testificate.Email(), testificate.passport());
+
+        assertThrows(Exception.class, () -> {
+            bookService.deleteItem(testificate.Email(), testificate.passport(), tickets.getFirst().id());
+        });
+    }
+
+    @AfterEach
+    void tearDown() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "Passenger", "book");
+    }
 }
