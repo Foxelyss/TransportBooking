2ae72fb6fac0650386ba304403032dc00f23d8cb
Степан Смирнов <foxelyss@gmail.com>
2025-04-10 15:13:59 +0700

    Обновление скрипта тестов

--- src/main/resources/data-booking-test.sql
+++ src/main/resources/data-booking-test.sql
@@ -13,5 +13,5 @@ insert into transportation values (null, 'Будущее', unixepoch()+86400, un
 insert into transportation values (null, 'Прошлое', unixepoch()-86400-86400, unixepoch()-86400, 1, 2, 1, 1, 26354, 50, 50);
 insert into transportation values (null, '123', unixepoch()+86400, unixepoch()+86400+86400, 1, 2, 1, 1, 26354, 2, 0);
 
-insert into passenger values (2, 123, 321,"unknown@example.com","Максим","Сидоров","");
-insert into book values (null,2,"Картой",123,2);
+insert into passenger values (2, 123, 321,'unknown@example.com','Максим','Сидоров','');
+insert into book values (null,2,'Картой',123,2);
